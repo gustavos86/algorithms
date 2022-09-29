372137644159b57bcefb373e084b4ca8c623f238
Hector Gustavo Serrano Gutierrez <gustavoserrano86@gmail.com>
2022-09-28 19:52:54 -0700

    implemented selection_sort algorithm

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,3 +1,7 @@
+"""
+I am studying sorting algorithms along with their Big Oh notation
+"""
+
 def bubble_sort(list1):
     """
     In Big Oh! worst case is quadratic O(N^2)
@@ -23,19 +27,38 @@ def bubble_sort(list1):
 
     return list1
 
-
-
-
 def selection_sort(list1):
     """
     In Big Oh! worst case is quadratic O(N^2)
     Altough in reality, it is slower than bubble_sort
     since there is only 1 swap action per iteration
     """
-    pass
+    list1 = list1.copy()
+
+    idx = 0
+    len_list1 = len(list1)
+
+    for i in range(idx, len_list1):
+        min_j = list1[i]
+        j_idx = 0
+        for j in range(idx + 1, len_list1):
+            if list1[j] < min_j:
+                min_j = list1[j]
+                j_idx = j
+        if j_idx:
+            list1[i], list1[j_idx] = list1[j_idx], list1[i]
+        idx += 1
+
+    return list1
 
 test = bubble_sort([2, 6, 8, 9, 1])
 print(test)
 
 test = bubble_sort([9, 8, 7, 6, 5])
+print(test)
+
+test = selection_sort([2, 6, 8, 9, 1])
+print(test)
+
+test = selection_sort([9, 8, 7, 6, 5])
 print(test)
